e73ad31c1566bb0f9e4c25609d64b9e077b00b0e
yash <yashkotadiya222@gmail.com>
2023-10-16 21:57:30 +0530

    add music palyer

---
+++ index.js
@@ -0,0 +1,155 @@
+var songs =[
+   
+    {
+        id:1,
+        imageSrc:"https://stat2.bollywoodhungama.in/wp-content/uploads/2017/06/Kedarnath-1.jpg",
+        audioSrc:"audios/Namo_Namo_-_Lyrical___Kedarnath___Sushant_Rajput___Sara_Ali_Khan___Amit_Trivedi___Amitabh_B(256k).mp3",
+        title:"Namo Namo - Kedarnath"
+    },
+
+    {
+        id:2,
+        imageSrc:"https://1.bp.blogspot.com/-j0B1TJlzF0Y/W9GytqZ2BXI/AAAAAAAAAbQ/JEA9ZcC8n78diu7iXd67i0NTpP4Eh7VyACLcBGAs/s1600/Front.jpg",
+        audioSrc:"audios/Full_Video__Galliyan_Song___Ek_Villain___Ankit_Tiwari___Sidharth_Malhotra___Shraddha_Kapoor(256k).mp3",
+        title:"Galliyan - Ek Villain"
+    },
+
+    {
+        id:3,
+        imageSrc:"https://2.bp.blogspot.com/-c4i46qywvqo/V8f2IWoYE2I/AAAAAAAAJk8/wVsKNBNeEU8nYeagMfZym64fvuAm_eZzQCLcB/s1600/Dangal%2BMovie%2BPoster%2BLatest.JPG",
+        audioSrc:"audios/Gilehriyaan_-_Lyrical_Video___Dangal___Aamir_Khan___Pritam___Amitabh_Bhattacharya(256k).mp3",
+        title:"Gilehriyaan - Dangal"
+    },
+
+    {
+        id:4,
+        imageSrc:"https://tse3.mm.bing.net/th?id=OIP.bvPSixo2RmR26vaheETAtAHaJ4&pid=Api&P=0&w=135&h=181",
+        audioSrc:"audios/Humraah_Full_Song-Lyrics___Malang___Sachet_Tandon_(256k).mp3",
+        title:"Humraah - Malang"
+    },
+
+    {
+        id:5,
+        imageSrc:"https://i.pinimg.com/originals/58/3b/59/583b59f16ea96421ec2f0b86dd925d26.jpg",
+        audioSrc:"audios/Nagada_Sang_Dhol___Full_Song___Goliyon_Ki_Rasleela_Ram-leela____Deepika_Padukone___Shreya_Ghoshal(256k).mp3",
+        title:"Nagada Sang Dhol - Ram Leela"
+    },
+
+    {
+        id:6,
+        imageSrc:"https://tse2.mm.bing.net/th?id=OIP.YmeLIhQrscdb56pqMauAFAHaJ4&pid=Api&P=0&w=154&h=206",
+        audioSrc:"audios/Jubin_Nautiyal_-_Raataan_Lambiyan__Lyrics_____Asees_Kaur____Tanishk_Bagchi____Shershaah(256k).mp3",
+        title:"Raataan Lambiyan - Shershaah"
+    },
+
+    {
+        id:7,
+        imageSrc:"https://bestoftheyear.in/wp-content/uploads/2016/03/Happy-New-Year-Poster.jpg",
+        audioSrc:"audios/OFFICIAL___Manwa_Laage__FULL_VIDEO_Song___Happy_New_Year___Shah_Rukh_Khan___Arijit_Singh(256k).mp3",
+        title:"Manwa Laage - Happy New Year"
+    },
+
+    {
+        id:8,
+        imageSrc:"https://tse4.mm.bing.net/th?id=OIP.LnDnjdptSybfgIrfirtDdAHaJ4&pid=Api&P=0&w=129&h=173",
+        audioSrc:"audios/Shayad_-_Love_Aaj_Kal___Kartik___Sara___Arushi___Pritam___Arijit_Singh(256k).mp3",
+        title:"Shayad - Love Aaj Kal"
+    },
+
+    {
+        id:9,
+        imageSrc:"https://fr.web.img2.acsta.net/pictures/19/03/18/10/16/2137399.jpg",
+        audioSrc:"audios/Teri_Mitti_-_Lyrical___Kesari___Akshay_Kumar___Parineeti_Chopra___Arko___B_Praak__Manoj_Muntashir(256k).mp3",
+        title:"Teri Mitti - Kesri"
+    },
+
+    {
+        id:10,
+        imageSrc:"https://4.bp.blogspot.com/-K_pRqKTHczY/WjP3bpecNmI/AAAAAAAANT8/2nMmrgpkbSkSTuasDgn-q3uao8gmp1bzQCLcBGAs/s1600/Tiger-Zinda-Hai-movie-poster-2.jpg",
+        audioSrc:"audios/Dil_Diyan_Gallan_Song__Tiger_Zinda_Hai__Salman_Khan,_Katrina_Kaif__Atif_Aslam__Vishal_&_Shekhar(256k).mp3",
+        title:"Dil Diyan Gallan - Tiger Zinda Hai"
+    },
+
+    {
+        id:11,
+        imageSrc:"https://loverays.com/movies/Sanam_Re_Movie-Poster-5.jpg",
+        audioSrc:"audios/Gazab_Ka_Hai_Ye_Din_Song_Full_Lyrics____Arijit_Singh____Amall_Malik____Sanam_Re___(256k).mp3",
+        title:"Gazab Ka Hai Yeh Din - Sanam Re"
+    },
+
+    {
+        id:12,
+        imageSrc:"https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2020/shiddat/shiddat-poster-9.jpg",
+        audioSrc:"audios/Jug_Jug_Jeeve_Song_Lyrics_English_Translation____Shiddat____Parampara_Tandon____Sachet_Tandon(256k).mp3",
+        title:"Jug Jug Jeeve - Shiddat"
+    },
+
+    {
+        id:13,
+        imageSrc:"https://www.theindianwire.com/wp-content/uploads/2019/04/Bharat-new-poster-2.jpg",
+        audioSrc:"audios/Full_Audio_CHASHNI_REPRISE___BHARAT___Salman_Khan___Katrina_Kaif___Vishal___ShekharFeat._Neha_Bhasin(128k).mp3",
+        title:"Chashni - Bharat"
+    },
+
+    {
+        id:14,
+        imageSrc:"http://www.game-ost.com/static/covers_soundtracks/2/4/243516_777971.jpg",
+        audioSrc:"audios/Luka_Chuppi__Duniyaa_Full_Video_Song___Kartik_Aaryan_Kriti_Sanon___Akhil___Dhvani_B(256k).mp3",
+        title:"Duniyaa - Luka Chupi"
+    },
+
+    {
+        id:15,
+        imageSrc:"https://i.pinimg.com/originals/fb/c4/02/fbc40235a6e796b8efb58a4e673e2e64.jpg",
+        audioSrc:"audios/Ik_Vaari_Aa_Full_Song___Raabta___Sushant_Singh_Rajput___Kriti_Sanon___Pritam_Arijit_Singh_Amitabh_B(256k).mp3",
+        title:"Ik Vaari Aa - Raabta"
+    },
+
+    {
+        id:16,
+        imageSrc:"https://media2.bollywoodhungama.in/wp-content/uploads/2016/07/A-Flying-Jatt-2-10-306x393.jpg",
+        audioSrc:"audios/Toota_Jo_Kabhi_Tara_-_Tiger_Shroff_,_Jacqueline__Atif_Aslam,_Sumedha_K__Sachin_Jigar(256k).mp3",
+        title:"Toota Jo Kabhi Tara - A Flying Jatt"
+    },
+
+    {
+        id:17,
+        imageSrc:"https://www.filmcompanion.in/wp-content/uploads/2020/07/F3F7776B-494A-4BFE-8FB4-B32F6B798D0A.jpeg",
+        audioSrc:"audios/Aashiqui_Aa_Gayi__Lyrics____Arijit_Singh___Radhe_Shyam___Mithoon___Lyrics_Video(256k).mp3",
+        title:"Aashiqui Aa Gayi - Radhe Shyam"
+    },
+
+    {
+        id:18,
+        imageSrc:"https://dsbihari.com/wp-content/uploads/2021/01/Bachchan-Pandey-Movie-Download-1-768x1086.jpg",
+        audioSrc:"audios/Heer_Raanjhana___Bachchan_Pandey___Lyrics(256k).mp3",
+        title:"Heer Raanjhana - Bachchhan Paandey"
+    },
+
+    {
+        id:19,
+        imageSrc:"https://mir-s3-cdn-cf.behance.net/project_modules/1400/a004bb66081713.5b0a50dcd57de.jpg",
+        audioSrc:"audios/Sanu_Ek_Pal_Chain_Video___Raid___Ajay_Devgn___Ileana_D_Cruz__Tanishk_B_Rahat_Fateh_Ali_Khan_Manoj_M(256k).mp3",
+        title:"Sanu Ek Pal Chain - Raid"
+    },
+
+    {
+        id:20,
+        imageSrc:"https://tse2.mm.bing.net/th?id=OIP.4LDVWG8gt316mt2WOvMfpAHaJ4&pid=Api&P=0&w=132&h=176",
+        audioSrc:"audios/Main_Ki_Karaan__Song___Laal_Singh_Chaddha___Aamir___Kareena___Sonu_Nigam__Pritam_Amitabh_Romy_Advait(256k).mp3",
+        title:"Main Ki Karaan - Laal Sinh Chaddha"
+    }
+
+]
+
+function playAudio(songid){
+
+ let song=songs.find(function(song){
+    return song.id===songid
+ });
+
+ document.getElementById("thumbnail").src=song.imageSrc;
+ document.getElementById("audio_player").src=song.audioSrc;
+ document.getElementById("audio_player_title").innerText=song.title;
+
+}
